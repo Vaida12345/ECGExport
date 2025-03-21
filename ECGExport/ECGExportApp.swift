da7fa65871435b3572be630434413a27d64e9a27
Vaida <developer@vaidaf.net>
2025-03-21 22:23:03 +1100

    Initial Commit

---
+++ ECGExport/ECGExportApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECGExportApp.swift
+//  ECGExport
+//
+//  Created by Vaida on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ECGExportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
